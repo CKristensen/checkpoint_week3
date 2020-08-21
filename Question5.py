74a3bb9fb8cd2d163dfc3ac7effc5bb4aa5d6009
C. Kristensen <cjckris@gmail.com>
2020-08-21 10:24:39 +0200

    q

--- Question5.py
+++ Question5.py
@@ -18,13 +18,42 @@ def shuffle_students():
     random.shuffle(students)
     return students
 
-#do the magic
 def assign_random_places_to_students():
     dic_seats = {}
     for s, p in zip(shuffle_students(), create_places()):
-        dic_seats[p] = s
+        dic_seats[s] = p
     return dic_seats
 
+def not_in_window(student, dic_places):
+    #windos are the seats ending in 2
+    if(dic_places[student][-1] == '2'):
+        old_seat = dic_places[student]
+        new_seat = dic_places[student][:-1] + '1'
+        for key in dic_places:
+            if(dic_places[key] == new_seat):
+                dic_places[key] = old_seat
+                dic_places[student] = new_seat
+    return dic_places
 
+def not_in_back(student, dic_places):
+    #windos are the seats ending in 3
+    if(int(dic_places[student][1]) == 2):
+        old_seat = dic_places[student]
+        new_seat = dic_places[student][0] + '1' + dic_places[student][2:]
+        for key in dic_places:
+            if(dic_places[key] == new_seat):
+                dic_places[key] = old_seat
+                dic_places[student] = new_seat
+    return dic_places
 
-print(assign_random_places_to_students())
+#do the magic
+def seat_arrange(s_not_window ='', s_not_in_back=''):
+    assign = assign_random_places_to_students()
+    assign = not_in_back(s_not_in_back, assign)
+    assign = not_in_window(s_not_window, assign)
+    return assign
+
+sa = seat_arrange(s_not_window='Carl', s_not_in_back='Malin')
+
+for key in sa:
+    print(key + ' seated ' +sa[key])
