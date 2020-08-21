5f05ea267e658f5d3414ecb9d18b4058ae08368a
C. Kristensen <cjckris@gmail.com>
2020-08-21 08:50:00 +0200

    question1

---
+++ Question1.py
@@ -0,0 +1,14 @@
+def fraud_detection(customers, salary, taxes):
+  fraud_list = []
+  for c, s, t in zip(customers, salary, taxes):
+
+    tax_perc = t/s
+    if((s >555000)&(tax_perc < 0.3)):
+      fraud_list.append(c)
+  return fraud_list
+
+customers = ['James’', '‘John’', '‘Robert’', '‘Mary’', '‘Patricia’', '‘Jennifer’', 'Carl']
+salary = [155000, 755000,  455000, 1255000, 635000, 575000, 555100]
+taxes = [55800, 317100, 182000, 3, 171450, 71400, 166000]
+
+print(fraud_detection(customers, salary, taxes))
